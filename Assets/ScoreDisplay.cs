95b5a1589ec3535c96f29f3d3dd068bc94aec388
Abraham <agm041214@gmail.com>
2022-11-20 16:01:27 +0300

    GameOverAnimator

--- Assets/ScoreDisplay.cs
+++ Assets/ScoreDisplay.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class ScoreDisplay : MonoBehaviour
 {
     private TMP_Text _text;
-    void Start()
+
+    private Animator _animator;
+    void Awake()
     {
-        _text = GetComponent<TMP_Text>();  
+        _text = GetComponent<TMP_Text>();
+        _animator = GetComponent<Animator>();
     }
 
     public void UpdateScore(int score)
     {
         _text.text = score.ToString();
+        if (_animator != null)
+            _animator.SetTrigger("ScoreUpdated");
     }
 
   
